16d35db75673394755095a1388c05a66a57772fe
jordan choquet <jordlepatineur@gmail.com>
2020-06-02 09:57:10 -0400

    débutFlècheParralel (abandon)

--- src/main/java/model/BlackArrowDouble.java
+++ src/main/java/model/BlackArrowDouble.java
@@ -16,7 +16,8 @@ public class BlackArrowDouble extends Arrow{
 	public Shape createShape() {
 		double angle = Math.atan2 (yEnd - yCoordinate, xEnd - xCoordinate) + Math.PI;
 	   
-	     
+		   double offset = 10;
+		
 	       double x1 = xEnd + 6 * Math.cos(angle - Math.PI/6);
 	       double y1 = yEnd + 6 * Math.sin(angle - Math.PI/6);
 	       double x2 = xEnd + 6 * Math.cos(angle + Math.PI/6);
@@ -27,16 +28,33 @@ public class BlackArrowDouble extends Arrow{
 	       double x4 = xCoordinate - 6 * Math.cos(angle + Math.PI/6);
 	       double y4 = yCoordinate - 6 * Math.sin(angle + Math.PI/6);
 	       
+	       double offsetX1 = offset * Math.cos(angle);
+	       double offsetY1 = offset * Math.sin(angle);
+	       double offsetX2 = offset * Math.cos(angle);
+	       double offsetY2 = offset * Math.sin(angle);
+	       
 	       Line mainLine = new Line();
+	       Line secLine = new Line();
 	       Line littleLine1 = new Line();
 	       Line littleLine2 = new Line();
 	       Line littleLine3 = new Line();
 	       Line littleLine4 = new Line();
+	       
 	       mainLine.setStartX(xCoordinate);
 	       mainLine.setStartY(yCoordinate);
 	       mainLine.setEndX(xEnd);
 	       mainLine.setEndY(yEnd);
 	       
+	       secLine.setStartX(xCoordinate);
+	       secLine.setStartY(yCoordinate);
+	       secLine.setEndX(xEnd);
+	       secLine.setEndY(yEnd);
+	       /*
+	       secLine.setStartX(xCoordinate - offsetX1);
+	       secLine.setStartY(yCoordinate + offsetY1);
+	       secLine.setEndX(xEnd - offsetX2);
+	       secLine.setEndY(yEnd + offsetY2);
+	       */
 	       littleLine1.setStartX(x1);
 	       littleLine1.setStartY(y1);
 	       littleLine1.setEndX(xEnd);
@@ -56,11 +74,25 @@ public class BlackArrowDouble extends Arrow{
 	       littleLine4.setStartY(y4);
 	       littleLine4.setEndX(xCoordinate);
 	       littleLine4.setEndY(yCoordinate);
+	       /*
+	       littleLine3.setStartX(x3-offsetX1);
+	       littleLine3.setStartY(y3+offsetY1);
+	       littleLine3.setEndX(xCoordinate - offsetX1);
+	       littleLine3.setEndY(yCoordinate + offsetY1);
 	       
+	       littleLine4.setStartX(x4 - offsetX1);
+	       littleLine4.setStartY(y4 + offsetY1);
+	       littleLine4.setEndX(xCoordinate - offsetX1);
+	       littleLine4.setEndY(yCoordinate + offsetY2);
+	       */
 	       Shape firstUnion = Shape.union(mainLine, littleLine1);
 	       Shape arrowHead = Shape.union(firstUnion, littleLine2);
 	       Shape arrowHeadA = Shape.union(arrowHead, littleLine3);
 	       Shape finalArrow = Shape.union(arrowHeadA, littleLine4);
+	       //Shape finalfinalArrow = Shape.union(finalArrow, secLine);
+	       //finalfinalArrow.setFill(Color.web(this.mainColor));
+	       //finalfinalArrow.setStroke(Color.web(this.borderColor));
+	       
 	       finalArrow.setFill(Color.web(this.mainColor));
 	       finalArrow.setStroke(Color.web(this.borderColor));
 	       
